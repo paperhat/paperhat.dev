19fae73ec5e3e2cde4c5884414d2a732178ee89e
Charles Munat <charles.munat@gmail.com>
2026-02-10 23:01:40 +1300

    utility commit

--- assets/js/prism-cdx.js
+++ assets/js/prism-cdx.js
@@ -10,7 +10,7 @@
   const lookupToken     = /~[a-z][A-Za-z0-9]*/;
 
   // Strings
-  const stringQuoted    = /"(?:\\.|[^\\"])*"/;
+  const stringQuoted    = /"(?:\\(?:\"|u[0-9A-Fa-f]{4}|u\{[0-9A-Fa-f]{1,6}\})|\\(?!\"|u[0-9A-Fa-f]{4}|u\{[0-9A-Fa-f]{1,6}\})|[^"\\])*"/;
   const stringBacktick  = /`[\s\S]*?`/;
 
   // Characters
